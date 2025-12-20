109f4257e3e9f329a3c735c10cccc09982e75012
RendonnM <srendonm@correo.iue.edu.co>
2025-12-20 10:46:36 -0500

    refactor(2025)🛠️: optimize challenge 19 using a map for route tracking

--- 2025/19/19.ts
+++ 2025/19/19.ts
@@ -1,17 +1,19 @@
 function revealSantaRoute(routes: string[][]): string[] {
-  let routeCopy = [...routes];
+  let routeMap = new Map<string, string>();
   const newRoute = [...routes[0]];
-
   let lastPosition = routes[0][1];
+
+  for (let i = 1; i < routes.length; i++) {
+    routeMap.set(routes[i][0], routes[i][1]);
+  }
+
   while (true) {
-    const element = routeCopy.find((el) => el[0] === lastPosition);
+    const element = routeMap.get(lastPosition);
     if (!element) break;
 
-    newRoute.push(element[1]);
-    lastPosition = element[1];
-
-    const i = routeCopy.indexOf(element);
-    routeCopy = [...routeCopy.slice(0, i), ...routeCopy.slice(i + 1)];
+    routeMap.delete(lastPosition);
+    newRoute.push(element);
+    lastPosition = element;
   }
 
   return newRoute;
